7c7926c85bcba11ecc4c571852103af96185aafb
Lucas Ferreira <129918519+LucasAndradeF@users.noreply.github.com>
2023-11-03 22:14:53 -0300

    Add atualizações

--- operadora.py
+++ operadora.py
@@ -1,7 +1,7 @@
 quantidade_minutos = int(input("Informe os minutos utilizados: "))
 
 if quantidade_minutos > 100:
-  total_minutos = 50 + ((quantidade_minutos % 100) * 2) 
+  total_minutos = 50 + ((quantidade_minutos - 100) * 2) 
   print(f"O valor a ser pago é: R$ {total_minutos:.2f}.")
 else:
-  print("O valor a ser pago é: R$ 50,00.")
+  print("O valor a ser pago é: R$ 50,00.") 
